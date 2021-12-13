8e2eeb21db03f44a9d708a5378cba9e74dd3c907
mrsamia <15203060@iubat.edu>
2021-12-13 16:30:59 +0600

    SRM | add_bootstrap.min.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import Button from "./component/Button";
 import Input from "./component/Input";
+import Nav from "./component/Nav";
+
+import "./style/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>React</h1>
-      <Button name="Submit" />
-      <Input type="password" />
+      <Nav />
+      <div className="container">
+        <h1>React</h1>
+        <Button name="Submit" />
+        <div>
+          <br></br>
+          <Input type="usename" placeholder="Usename" />
+        </div>
+        <br></br>
+        <div>
+          <Input type="adress" placeholder="Adress" />
+        </div>
+        <br></br>
+        <div>
+          <Input type="number" placeholder="Number" />
+        </div>
+        <br></br>
+        <div>
+          <Input type="password" placeholder="Password" />
+        </div>
+        <br></br>
+      </div>
     </div>
   );
 }
