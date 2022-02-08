990e5a680026b7617eedc75b63365ce28be6b202
mrsamia <15203060@iubat.edu>
2022-02-08 13:33:54 +0600

    SRM | button_click

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "../style/button.css";
 
-function Button({ name }) {
+function Button({ name}) {
+
+  function click(){
+    alert("clicked");
+  }
+
   return (
+
     <div>
-      <button className="btn_style">{name}</button>
+      <button className="btn_style" onClick={click}>{name}</button>
     </div>
+    
   );
 }
 export default Button;
